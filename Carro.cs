772072cce07944757d248a0412c3d18701c64f4b
Eduardosm06 <mouraoe243@gmail.com>
2024-09-23 17:18:00 -0300

    Adicionar arquivos de projeto.

---
+++ Carro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace GerenciadorDeVeiculos
+{
+    public class Carro: Veiculo
+    {
+        public int NumeroDePortas { get; set; }
+        public Carro(int numeroDePortas) : base(marca,modelo,ano,combustivel)
+        {
+            NumeroDePortas = numeroDePortas;
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Veiculo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeVeiculos
+{
+    public class Veiculo
+    {
+        public string Marca { get; set; }
+
+        public string Modelo { get; set; }
+
+        public int Ano { get; set; }
+
+        public string Combustivel { get; set; }
+
+       public Veiculo(string marca,string modelo,int ano,string combustivel ) 
+        {
+            Marca = marca;  
+            Modelo = modelo;    
+            Ano = ano; 
+            Combustivel= combustivel;
+        }   
+        public virtual double CaucularEficiencia()
+        {
+            return 0; 
+        }
+        public virtual void MostrarDetalhes()
+        {
+
+        }
+
+    }
+}
